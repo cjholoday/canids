af6a16cf2f2a5f64c7b8ed139fb187565fb74230
Janko <dubrajoh@umich.edu>
2015-10-30 15:08:23 -0400

    Added comments in mapping.cpp

--- mapping.cpp
+++ mapping.cpp
@@ -18,19 +18,24 @@ int main(){
 	double frequency;
 	string identifierCAN;
 	unordered_map<string, double> map{,0};
+// initialize the variables and the map
 	file.open("/sd/messagestore.txt");
+// open the message file on the sd card
 	
 	if(!file.open("/sd/messagestore.txt")){
 		lcd.printf("file open failed %d", errno);
 		exit(1);
+	// if file doesn't open then print to LCD and close program
 	}
 
 	while (file >> identifierCAN) {
 		file >> frequency;
-		map.insert({identifierCAN, frequency});	
+		map.insert({identifierCAN, frequency});
+	// insert the CAN Id and the frequency into the map from text file	
 	}
 	
 	file.close();
+	// close file
 	
 	return 0;
 }
