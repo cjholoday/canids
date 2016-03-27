4daaf0556da753e425550a20dc257d9024852876
xiesusan <susanxie@sjtu.edu.cn>
2016-03-27 16:06:48 -0400

    modifying main

--- detection/main.cpp
+++ detection/main.cpp
@@ -1,31 +1,25 @@
-/*
-
-mbed Can-Bus demo
-
-This program is to demonstrate the CAN-bus capability of the mbed module.
-
-http://www.skpang.co.uk/catalog/product_info.php?products_id=741
-
-v1.0 July 2010
-
-********************************************************************************
-
-WARNING: Use at your own risk, sadly this software comes with no guarantees.
-This software is provided 'free' and in good faith, but the author does not
-accept liability for any damage arising from its use.
-
-********************************************************************************
-
-
-*/
-
 #include "globals.h"
 #include "detect.h"
-
+#include "learning.h"
+#include "stdio.h"
 
 int main() {
 
     printf("Automotive IDS initializing...");
+    int msgReaderErr = messageReader();
+    switch (msgReaderErr){
+	    case 0:
+	    break;
+	    case 1:
+	    //can read error
+	    break;
+	    case 2:
+	    //file open error
+	    break;
+	    default:
+	    break;
+    }
+
 
     return 0;
 }
