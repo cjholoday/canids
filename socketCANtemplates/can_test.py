17689f370c198aa10b2c9a862df361d41e443694
lynshi <lyndrive@gmail.com>
2017-03-28 23:24:44 -0400

    Using recv function now

--- socketCANtemplates/can_test.py
+++ socketCANtemplates/can_test.py
@@ -1,9 +1,14 @@
+# Reads and prints messages on the CAN bus to the terminal
+# Includes time stamp, message ID, data length code, message content
+
 import can
-# can.rc['interface'] = 'socketcan_native' # for python 3.3+
 can.rc['interface'] = 'socketcan_native'
+# can.rc['interface'] = 'socketcan_ctypes'
+# socketcan_ctypes should be used for older Linux kernels though
+#   both native and ctypes seem to work on Python 3.3+
 can.rc['channel'] = 'vcan0'
 from can.interfaces.interface import Bus
 
 bus = Bus()
-for msg in bus:
-    print(msg.data)
+while(1):
+    print(bus.recv())
