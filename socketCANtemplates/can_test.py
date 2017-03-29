5da4814bd5aca6c3694b8bb9fccddd4e3f8bb5e5
lynshi <lyndrive@gmail.com>
2017-03-29 00:38:39 -0400

    Using BufferedReader

--- socketCANtemplates/can_test.py
+++ socketCANtemplates/can_test.py
@@ -6,9 +6,21 @@ can.rc['interface'] = 'socketcan_native'
 # can.rc['interface'] = 'socketcan_ctypes'
 # socketcan_ctypes should be used for older Linux kernels though
 #   both native and ctypes seem to work on Python 3.3+
-can.rc['channel'] = 'vcan0'
+#can.rc['channel'] = 'vcan0'
 from can.interfaces.interface import Bus
 
-bus = Bus()
+bus1 = Bus(channel='vcan0')
+listener1 = can.BufferedReader()
+notifier1 = can.Notifier(bus1,[listener1])
+
 while(1):
-    print(bus.recv())
+    print("This is the message: ", listener1.get_message(10))
+    # Puts message in a queue
+    input()
+
+"""
+while(1):
+    bus.flush_tx_buffer()
+    message = bus.recv()
+    print(message.dlc, message.data)
+"""
