bea3ced1ad7e3b0ce334b1a342eae22c5ca56fa9
Colton Holoday <choloday@umich.edu>
2017-09-20 21:31:56 -0400

    Plan out attack file structure

---
+++ attack.py
@@ -0,0 +1,10 @@
+# attack.py
+#
+# purpose: provides an interface to all attack types
+# note: attack.py must be placed in the top level directory so that the attack
+#       submodule has access to other submodules' functions
+
+from attack import attacks
+
+if __name__ == '__main__':
+    attacks.hello_world()
---
+++ attack/attacks.py
@@ -0,0 +1,8 @@
+# attacks.py
+#
+# purpose: implements all attack types
+
+from data import utils
+
+def hello_world():
+    print('Hello World')
