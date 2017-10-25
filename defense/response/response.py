2ec0fac6fc9457e04684859fbb7d9e2e49c77463
Zachary <zcavazos@umich.edu>
2017-10-25 16:39:02 -0400

    response.py

--- defense/response/response.py
+++ defense/response/response.py
@@ -9,15 +9,16 @@ from random import uniform
 
 channel_in = sys.argv[1]
 bitrate = int(sys.argv[2])
-send_recv = sys.argv[3]
-id_in = int(sys.argv[4])
-length = int(sys.argv[5])
-data = int(sys.argv[6])
+#send_recv = sys.argv[3]
+#id_in = int(sys.argv[4])
+#length = int(sys.argv[5])
+#data = int(sys.argv[6])
 
 can.rc['interface'] = 'socketcan_ctypes'
 can.rc['channel'] = channel_in
 bus = Bus()
 
+send_recv = "receive"
 out = [channel_in, " | ", send_recv, " | ", "bitrate: ", str(bitrate), " | ", "count: ", "" ]
 
 def clean(*args):
@@ -27,21 +28,34 @@ def clean(*args):
 if __name__ == '__main__':
 
     signal(SIGTERM, clean)
+    
+    consecutive_messages = 1;
+    last_message_id = -1;
 
-    count = 0
+    while 1:
+        if(send_recv == "receive"):
+            while 1:
+                temp_msg = bus.recv();
+                id = temp_msg.arbitration_id;
+                if id == last_message_id:
+                    consecutive_messages += 1
+                else:
+                    consecutive_messages = 1
+                last_message_id = id
+                print(consecutive_messages)
+                if consecutive_messages == 10:
+                    print("5 consecutive messages with same ID")
+                    send_recv = "send"
+                    break;
+                    #consecutive_messages = 0
+                #print(bus.recv())
+                #count += 1
+                #out[8] = str(count)    
 
-    if(send_recv == "send"):
-        message = can.Message(extended_id = False, is_error_frame = False, arbitration_id = id_in, data = [data]*length)
-        while 1:
-            sleep(uniform(0.001,0.1))
-            bus.send(message)
-            count += 1
-            out[8] = str(count)
+        elif(send_recv == "send"):
+            message = can.Message(extended_id = False, is_error_frame = False, arbitration_id = last_message_id, data = [0])
+            while 1:
+                sleep(uniform(0.001,0.1))
+                bus.send(message)
 
-    elif(send_recv == "receive"):
-        print()
-        while 1:
-            print(bus.recv())
-            count += 1
-            out[8] = str(count)
-~                                 
+
